7244e8442b4bac4c641ee6862892af0bb6007cab
Adam Stehlík <stehlikadam@gmail.com>
2022-10-31 11:07:43 +0100

    Initial Commit

---
+++ Elons Rockets/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Elons Rockets
+//
+//  Created by adam.stehlik on 31/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
