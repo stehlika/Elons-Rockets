76bf734522a6458a540a8f311ec6be841f5d20d0
Adam Stehlik <stehlikadam@gmail.com>
2022-11-04 12:26:02 +0100

    [FEATURE] Added root view controller

---
+++ Elons Rockets/SupportingFiles/SceneDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  SceneDelegate.swift
+//  Elons Rockets
+//
+//  Created by adam.stehlik on 31/10/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let appCoordinator = AppCoordinator()
+
+        window?.rootViewController = appCoordinator.rootViewController
+        window?.makeKeyAndVisible()
+    }
+}
