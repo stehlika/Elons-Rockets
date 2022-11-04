5756fc38f42b3e1f4909565eb3f2499a8578d28c
Adam Stehlik <stehlikadam@gmail.com>
2022-11-04 12:26:18 +0100

    [CHORE] Added custom constants

---
+++ Elons Rockets/SupportingFiles/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  Elons Rockets
+//
+//  Created by adam.stehlik on 03/11/2022.
+//
+
+import Foundation
+
+// TODO: Think about using apple Measurement 
+
+// 1 meter is 3.28084 feet
+public let metersToFeetRatio: Float = 3.28084
+
+// 1 kg is appx 2.205 lb
+public let kgsToLbsRatio: Float = 2.205 
